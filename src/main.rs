aa9956ab32caf91ea362a79302d1cdb7f0c4c377
AJTJ <janke.aaron@gmail.com>
2021-04-29 15:27:05 -0400

    db created, connected and queried

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use actix::{Actor, StreamHandler};
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
-// use mysql::prelude::*;
-// use mysql::*;
+use serde_json::json;
+use sqlx::sqlite::SqlitePoolOptions;
+use sqlx::FromRow;
 
-struct MessageBlock {
-    user_name: String,
-    user_id: String,
-    message: String,
-    message_id: String,
-}
+// USAGE NOTES
+
+// struct MessageBlock {
+//     user_name: String,
+//     user_id: String,
+//     message: String,
+//     message_id: String,
+// }
 
 struct TheWebSocket;
 
@@ -46,6 +49,32 @@ async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, E
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let test_data = json!({
+        "hello": "this"
+    });
+
+    println!("{:?}", test_data);
+
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect("sqlite://./chat.db")
+        .await
+        .expect("pool FAILURE");
+
+    #[derive(FromRow, Debug)]
+    struct User {
+        id: i64,
+        name: String,
+    }
+
+    let all_users: Vec<User> = sqlx::query_as("SELECT * from user")
+        .fetch_all(&pool)
+        .await
+        .expect("dang thing");
+
+    println!("pool: {:?}", pool);
+    println!("all_users: {:?}", all_users);
+
     HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
         .bind("127.0.0.1:8081")?
         .run()
