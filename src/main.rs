96ff6fa509cd799de035cfe46bd1791857d78592
AJTJ <janke.aaron@gmail.com>
2021-05-02 13:06:47 -0400

    now receiving message from frontend

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use actix_web_actors::ws;
 use chrono;
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
-use sqlx::{Sqlite, sqlite::SqlitePoolOptions};
-use sqlx::Pool;
+use serde_json::{json, Value};
+use sqlx::{sqlite::SqlitePoolOptions, Sqlite};
+use sqlx::{Pool, SqlitePool};
 // use sqlx::FromRow;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -30,14 +30,15 @@ struct Room {
 }
 
 struct WebSockActor {
-    db_pool: Pool<Sqlite>,
-};
+    all_messages: serde_json::Value,
+}
 
 impl Actor for WebSockActor {
     type Context = ws::WebsocketContext<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
-        ctx.text("HEY OVER HERE".to_string());
+        println!("STARTING HERE");
+        ctx.text(self.all_messages.to_string());
     }
 
     fn stopping(&mut self, _: &mut Self::Context) -> Running {
@@ -46,34 +47,41 @@ impl Actor for WebSockActor {
     }
 }
 
-fn text_handler(text: &String) -> &str {
-    match text.as_str() {
-        "hey" => "hello",
-        "dog" => "cat",
-        x => x,
-    }
+/**
+TODO HERE
+add current time object to received message
+save message in database
+*/
+
+fn message_handler(json_message: &String) -> &str {
+    println!("{:?}", json_message);
+    let mes: Value = serde_json::from_str(&json_message).expect("parsing json msg");
+    println!("{:?}", mes);
+
+    r#"[{"message": 43}]"#
 }
 
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSockActor {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         match msg {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text_handler(&text)),
+            Ok(ws::Message::Text(json_message)) => ctx.text(message_handler(&json_message)),
             Ok(ws::Message::Binary(_)) => println!("Unexpected binary"),
             _ => (),
         }
     }
 }
-
-async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let db_pool = SqlitePoolOptions::new()
-        .max_connections(5)
-        .connect("sqlite://./chat.db")
-        .await
-        .expect("pool FAILURE");
-
+/**
+TODO HERE
+Perform left outer join to get user name from user table
+*/
+async fn index(
+    db_pool: web::Data<SqlitePool>,
+    req: HttpRequest,
+    stream: web::Payload,
+) -> Result<HttpResponse, Error> {
     let all_messages: Vec<Message> = sqlx::query_as!(Message, "SELECT * from message")
-        .fetch_all(&db_pool)
+        .fetch_all(db_pool.get_ref())
         .await
         .expect("all messages failure");
 
@@ -81,7 +89,13 @@ async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, E
 
     println!("all_messages: {:?}", all_messages_json);
 
-    let resp = ws::start(WebSockActor {db_pool: db_pool.clone()}, &req, stream);
+    let resp = ws::start(
+        WebSockActor {
+            all_messages: all_messages_json,
+        },
+        &req,
+        stream,
+    );
     println!("{:?}", resp);
     resp
 }
@@ -98,8 +112,22 @@ async fn main() -> std::io::Result<()> {
     // set env variables for sqlx
     dotenv().ok();
 
-    HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
-        .bind("127.0.0.1:8081")?
-        .run()
+    // pool constructed here for all actors
+    let db_pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect("sqlite://./chat.db")
         .await
+        .expect("pool FAILURE");
+
+    let shared_db_pool = web::Data::new(db_pool);
+
+    HttpServer::new(move || {
+        App::new()
+            // pass a clone of the pool to the request
+            .app_data(shared_db_pool.clone())
+            .route("/ws/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8081")?
+    .run()
+    .await
 }
